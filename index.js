328431f54e3de25e0e8eee2827ef51cf4ff0d1d3
Killian <killianperea@gmail.com>
2022-02-24 00:15:55 +0100

    add pdf generator

---
+++ index.js
@@ -0,0 +1,59 @@
+import puppeteer from 'puppeteer';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+export class ExportHtmlToPDF {
+
+    constructor() {
+        this._generatePdf();
+    }
+
+    _getTemplateHtml() {
+        console.log("Loading template file in memory")
+        try {
+            const invoicePath = resolve("cv/index.html");
+            return readFileSync(invoicePath, { encoding: 'utf8' });
+        } catch (err) {
+            return new Error("Could not load html template");
+        }
+    }
+
+    async _generatePdf() {
+        const cv = this._getTemplateHtml();
+
+        const browser = await puppeteer.launch({
+            headless: true
+        });
+
+        const page = await browser.newPage();
+
+        await page.setContent(cv, {
+            waitUntil: 'domcontentloaded'
+        });
+
+        await page.addStyleTag({
+            path: 'cv/index.css',
+            waitUntil: 'domcontentloaded'
+        });
+
+        // We use pdf function to generate the pdf in the same folder as this file.
+        await page.pdf({
+            path: 'Curriculum_Vitae_Killian_Perea_Ruiz.pdf',
+            format: 'A4',
+            printBackground: true,
+            margin: {
+                left: '0px',
+                top: '0px',
+                right: '0px',
+                bottom: '0px'
+            }
+        });
+
+        await browser.close();
+
+        console.log("PDF Generated");
+    }
+}
+
+
+new ExportHtmlToPDF();

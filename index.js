9505bf229f929a8b9d42ad5ca124a7a35af3b9c6
Killian <killianperea@gmail.com>
2022-02-25 00:59:31 +0100

    create server tu get local files

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import puppeteer from 'puppeteer';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
+import express from 'express';
 
 export class ExportHtmlToPDF {
 
+    server = null;
+
     constructor() {
+
+        const app = express();
+        const port = 3000;
+
+        app.use(express.static('cv'));
+
+        this.server = app.listen(port, function (err) {
+            if (err) console.log('Error in Webserver setup')
+            console.log(`Webserver listening at http://localhost:${port}`);
+        });
         this._generatePdf();
     }
 
@@ -22,16 +35,20 @@ export class ExportHtmlToPDF {
         const cv = this._getTemplateHtml();
 
         const browser = await puppeteer.launch({
-            headless: true
+            headless: true,
+            args: ['--no-sandbox']
         });
 
         const page = await browser.newPage();
 
-        await page.setContent(cv, { waitUntil: 'domcontentloaded' });
+        await page.setContent(cv, { waitUntil: "networkidle0" });
 
         // Add Fonts
         await page.addStyleTag({
-           url: 'https://fonts.googleapis.com/css?family=Heebo'
+            path: 'cv/fonts/font.css'
+        });
+        await page.addScriptTag({
+            url: 'https://kit.fontawesome.com/413752956c.js'
         });
 
         // Add CSS
@@ -40,11 +57,11 @@ export class ExportHtmlToPDF {
         });
 
         await page.addStyleTag({
-            path: 'cv/css/left-side.css',
+            path: 'cv/css/left-side.css'
         });
 
         await page.addStyleTag({
-            path: 'cv/css/right-side.css',
+            path: 'cv/css/right-side.css'
         });
 
         await page.evaluateHandle('document.fonts.ready');
@@ -65,6 +82,10 @@ export class ExportHtmlToPDF {
         await browser.close();
 
         console.log("PDF Generated");
+
+        await this.server.close(() => {
+            console.log('Stopping webserver.')
+        });
     }
 }
 
